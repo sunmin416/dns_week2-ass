34dcb59821f3ef102ce9b4fb06a8fbbe786c3f04
sunmin416 <rhtjsals0106@gmail.com>
2026-02-03 18:59:02 +0900

    [FIX] 응답 파싱 에러 해결

--- src/main/java/gdg/DNS/dto/CloudflareApiRes.java
+++ src/main/java/gdg/DNS/dto/CloudflareApiRes.java
@@ -1,12 +1,14 @@
 package gdg.DNS.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudflareApiRes<T> {
     private Boolean success;
-    private List<String> errors;
-    private List<String> messages;
+    private List<Object> errors;
+    private List<Object> messages;
     private T result;
 }
